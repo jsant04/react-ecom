fcb0357dbf705ee550a7525b5e54be401339a0ae
jsant04 <nash.santiago04@gmail.com>
2025-05-29 12:33:53 +0800

    added sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,52 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7MewIVsH7Cuzr3vsBDajHp5BKHSBMqe8",
+    authDomain: "ecom-website-90386.firebaseapp.com",
+    projectId: "ecom-website-90386",
+    storageBucket: "ecom-website-90386.firebasestorage.app",
+    messagingSenderId: "99881451265",
+    appId: "1:99881451265:web:425459fb582a4b661ae500"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+        await setDoc(userDocRef, {
+            displayName,
+            email,
+            createdAt
+        })
+
+    } catch (error) {
+        console.log('error creating the user', error.message)
+     }
+    }
+    return userDocRef;
+}
+
+
